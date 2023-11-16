e35258567f4356d7b3071ae491baf5bf9984b825
Geeky-eddie <edidiongharrison18@gmail.com>
2023-11-16 15:30:35 -0800

    new monty update

--- add.c
+++ add.c
@@ -1,32 +1,36 @@
 #include "monty.h"
-/**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_add(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+/**
+ * add_top_two_elements - Adds the top two elements of the stack.
+ * @stack_head: Pointer to the stack head.
+ * @line_number: Line number in the file.
+ * Return: No return value.
+ */
+void add_top_two_elements(stack_t **stack_head, unsigned int line_number)
+{
+    stack_t *current;
+    int stack_length = 0, sum;
+
+    current = *stack_head;
+
+    while (current)
+    {
+        current = current->next;
+        stack_length++;
+    }
+
+    if (stack_length < 2)
+    {
+        fprintf(stderr, "L%d: Unable to add, stack is too short\n", line_number);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*stack_head);
+        exit(EXIT_FAILURE);
+    }
+
+    current = *stack_head;
+    sum = current->n + current->next->n;
+    current->next->n = sum;
+    *stack_head = current->next;
+    free(current);
 }
